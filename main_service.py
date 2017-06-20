eef310320cc6fadd7cca251dbe39821ef0c7a48b
Comm4ndo <marcobaldanza@hotmail.co.uk>
2017-06-20 21:44:01 +0100

    feat(imgur): now handels images posted to imgur

--- main_service.py
+++ main_service.py
@@ -7,6 +7,7 @@ from random import randint
 import time
 import os
 from configparser import ConfigParser
+from bs4 import BeautifulSoup
 
 img_ext = ('.jpg','.jpeg','.png','.bmp')
 urls = []
@@ -23,14 +24,25 @@ def main():
         url = urls[rand_num-1]
         filename, file_extension = os.path.splitext(url)
         if file_extension in img_ext:
-            urllib.request.urlretrieve(url, "img")
-            change_image_ubuntu = '/usr/bin/gsettings set org.gnome.desktop.background picture-uri "file:'
-            img_path = os.getcwd() + '/img"'
-            command_combined = change_image_ubuntu + img_path
-            os.system(command_combined)
-            time.sleep(int(Config.get('settings', 'interval')))
+            set_bg(url)
+        elif "imgur" in url:
+            r = urllib.request.urlopen(url)
+            soup = BeautifulSoup(r, "lxml")
+            res = soup.find_all(class_="zoom")
+            for link in res:
+                srcurl = link.get("href")
+                srcurl = "http://" + srcurl[2:]
+                set_bg(srcurl)
         else:
             continue
+
+def set_bg(url):
+    urllib.request.urlretrieve(url, "img")
+    change_image_ubuntu = '/usr/bin/gsettings set org.gnome.desktop.background picture-uri "file:'
+    img_path = os.getcwd() + '/img"'
+    command_combined = change_image_ubuntu + img_path
+    os.system(command_combined)
+    time.sleep(int(Config.get('settings', 'interval')))
 
 def get_urls(reddit):
     rand_num = randint(1, int(Config.get('settings', 'sub_count')))
